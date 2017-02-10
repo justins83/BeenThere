9eb74a3a14b1f958f4a374a5c9191f20f0ed7af3
justins83 <justins83.waze@gmail.com>
2017-02-10 16:50:27 -0500

    Adding WazeWrap require
    Adding WazeWrap require - an older version is being used in some instances

--- BeenThere.js
+++ BeenThere.js
@@ -7,7 +7,8 @@
 // @include             https://beta.waze.com/*
 // @require             https://greasyfork.org/scripts/27023-jscolor/code/JSColor.js
 // @require             https://greasyfork.org/scripts/27254-clipboard-js/code/clipboardjs.js
-// @version             0.4.1
+// @require             https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
+// @version             0.4.2
 // @grant               none
 // ==/UserScript==
 //---------------------------------------------------------------------------------------
